2e69e12e9fc850c166b401600b01bb3a13a0e196
Bruno_Cheng <Bruno_cheng@163.com>
2019-12-03 19:06:34 +0800

    更新了callable获取返回值的描述

--- upgrade_concurrent/src/main/java/com/suixin/concurrent/L1线程间共享和协作/L1启动和终止线程/L1三种启动线程的方式/NewThreadStart.java
+++ upgrade_concurrent/src/main/java/com/suixin/concurrent/L1线程间共享和协作/L1启动和终止线程/L1三种启动线程的方式/NewThreadStart.java
@@ -8,34 +8,37 @@ import java.util.concurrent.*;
  */
 public class NewThreadStart {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
+        System.out.println(Thread.currentThread().getName());//注意，这里打印的是主线程的名字：main
+        //1、继承Thread类
         NewThreadByThread newThreadByThread = new NewThreadByThread();
-        newThreadByThread.run();
-        System.out.println(Thread.currentThread().getName());//main
+        newThreadByThread.run();//执行重写的run方法，打印继承Thread开始的线程
+        System.out.println(newThreadByThread.getName());//Thread-0
 
+        //2、实现Runnable接口
         NewThreadByRunnable newThreadByRunnable = new NewThreadByRunnable();
         //创建Runnable实现类的实例，并以此实例作为Thread的target对象，即该Thread对象才是真正的线程对象
         new Thread(newThreadByRunnable).start();
 
-
+        //3、实现Callable接口
         NewThreadByCallable newThreadByCallable = new NewThreadByCallable();
         //Callable与Runnable不同,Thread方法不知此Callable,为了将Callable放给Thread,将Callable包装成Runnable
         //Callable可呼叫的  Future未来，task任务
         FutureTask<String> stringFutureTask = new FutureTask<>(newThreadByCallable);
+        //启动线程的方式
         new Thread(stringFutureTask).start();
 
-        //而且可以获取Callable返回的对象值
-        //启动的时候Thread线程是没有输出项的，需要自己获取
+        //启动的时候Thread线程是没有输出项的，需要自己获取Callbale的返回值
         System.out.println(stringFutureTask.get());
 
-        //多线程情况下获取Callable返回的对象值
+        //注意：FutureTask方式只能一个个的启动，对于多个线程情况比较麻烦
+        //此时使用Executors来启动多个线程
         ExecutorService es = Executors.newFixedThreadPool(2);
         Future<String> future1 = es.submit(newThreadByCallable);
         Future<String> future2 = es.submit(newThreadByCallable);
         String s1 = future1.get();
         String s2 = future2.get();
-        System.out.println(s1+","+s2);
-        //记得关闭线程
+        System.out.println(s1==s2);//true
+        //使用Executors记得关闭线程
         es.shutdown();
-
     }
 }
--- upgrade_concurrent/src/main/java/com/suixin/concurrent/L1线程间共享和协作/L1启动和终止线程/L1三种启动线程的方式/TestCallable.java
+++ upgrade_concurrent/src/main/java/com/suixin/concurrent/L1线程间共享和协作/L1启动和终止线程/L1三种启动线程的方式/TestCallable.java
@@ -1,6 +1,7 @@
-package com.suixin;
+package com.suixin.concurrent.L1线程间共享和协作.L1启动和终止线程.L1三种启动线程的方式;
 
 import com.suixin.concurrent.L1线程间共享和协作.L3线程间共享.L2Volatile关键字.Singleton;
+import com.suixin.concurrent.L1线程间共享和协作.L3线程间共享.L2Volatile关键字.TestSingleton;
 
 import java.util.concurrent.*;
 
@@ -11,13 +12,15 @@ import java.util.concurrent.*;
  */
 public class TestCallable {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        Callable<Singleton> callable= new Callable<Singleton>() {
+        Callable<Singleton> callable = new Callable<Singleton>() {
             @Override
             public Singleton call() throws Exception {
+                System.out.println("返回单例");
                 return Singleton.getSingleton();
             }
         };
-        //Future<TestSingleton> f1 =  new Future<callable>();不能执行
+
+
         //需要放在线程池里面才可以
         ExecutorService es = Executors.newFixedThreadPool(2);
         Future<Singleton> f1 = es.submit(callable);
@@ -28,5 +31,16 @@ public class TestCallable {
         System.out.println(s1);
         System.out.println(s2);//地址值相同
         es.shutdown();//关闭线程服务
+
+        //
+        FutureTask<Singleton> f3 = new FutureTask<>(callable);
+        FutureTask<Singleton> f4 = new FutureTask<>(callable);
+        Thread t1 = new Thread(f3);
+        t1.start();
+        Thread t2 = new Thread(f4);
+        t2.start();
+        Thread.sleep(100);
+        System.out.println(f3.get());
+        System.out.println(f4.get());
     }
 }
